28b1943ea99893f2f0cd535defd33e767be2de58
”Ozi <31310132+OziOcb@users.noreply.github.com>
2020-02-29 23:04:57 +0000

    Master Commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const t1 = performance.now();
+const t2 = performance.now();
+console.log("result_01 \n ->", t2 - t1);
+
+const t3 = performance.now();
+const t4 = performance.now();
+console.log("result_02 \n ->", t4 - t3);
